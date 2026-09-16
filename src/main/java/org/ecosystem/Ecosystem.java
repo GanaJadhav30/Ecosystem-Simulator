8e22190a12667aa2be33f3d6da69e877c6086067
GanaJadhav30 <jadhavganeshatclg@gmail.com>
2026-09-16 21:40:20 +0530

    add plants

--- src/main/java/org/ecosystem/Ecosystem.java
+++ src/main/java/org/ecosystem/Ecosystem.java
@@ -8,10 +8,13 @@ import java.util.List;
 public class Ecosystem {
     private final World world;
     private final List<Animal> animals;
+    private final List<Plant> plants;
 
     public Ecosystem(){
         world = new World();
         animals = new ArrayList<>();
+        plants = new ArrayList<>();
+        //creates rabbit
         for (int i = 0; i < 20; i++) {
             double x = Math.random()*World.width;
             double y = Math.random()*World.length;
@@ -19,6 +22,16 @@ public class Ecosystem {
             Rabbit rabbit = new Rabbit(x,y);
             animals.add(rabbit);
             world.getChildren().add(rabbit.getGraphic());
+        }
+
+        //create plants
+        for (int i = 0; i < 50; i++) {
+            double x = Math.random()*World.width;
+            double y = Math.random()*World.length;
+
+            Plant plant = new Plant(x,y);
+            plants.add(plant);
+            world.getChildren().add(plant.getGraphic());
         }
 
     }
---
+++ src/main/java/org/ecosystem/Plant.java
@@ -0,0 +1,36 @@
+package org.ecosystem;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+public class Plant {
+    private double x;
+    private double y;
+
+    private final Circle graphic;
+
+    public Plant(double x,double y){
+        this.x = x;
+        this.y = y;
+
+        graphic = new Circle(5, Color.DARKGREEN);
+        updateGraphic();
+    }
+
+    private void updateGraphic(){
+        graphic.setCenterX(x);
+        graphic.setCenterY(y);
+    }
+
+    public double getX(){
+        return x;
+    }
+
+    public double getY(){
+        return y;
+    }
+
+    public Circle getGraphic(){
+        return graphic;
+    }
+}
